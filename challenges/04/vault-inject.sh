1711a4b5ee51198e52b2a17b58729ce5de25acb9
Antonio Piedra <35215298+ajpiedra@users.noreply.github.com>
2025-03-14 22:05:35 +0100

    Delete challenges/04/vault-inject.sh

--- challenges/04/vault-inject.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export DB_USER=$(vault kv get -field=username secrets/database)
-export DB_PASSWORD=$(vault kv get -field=password secrets/database)
-exec "$@" #execute the command passed to the script.
